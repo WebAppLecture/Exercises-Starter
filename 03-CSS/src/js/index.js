e5231760b184ac9aaff8c86afbeb1332160287dc
Neon-Ape <louis@pixelwizart.de>
2020-04-29 00:55:38 +0200

    Add industrial skin

--- 03-CSS/src/js/index.js
+++ 03-CSS/src/js/index.js
@@ -1,7 +1,7 @@
 import { SkinChanger } from "./SkinChanger.js";
 
 let skinStyle = document.querySelector("#skin"),
-    skins = ["gold","peach","basic","win95","mech","scifi","gamble"];
+    skins = ["gold","peach","basic","win95","mech","scifi","gamble","industrial"];
 
 window.skinChanger = new SkinChanger(skinStyle, skins, "./src/css/");
 
