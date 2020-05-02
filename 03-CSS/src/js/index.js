914568fa811eed5b90bfea3a217185c11f3410de
Neon-Ape <louis@pixelwizart.de>
2020-05-02 23:21:44 +0200

    Add moiree skin

--- 03-CSS/src/js/index.js
+++ 03-CSS/src/js/index.js
@@ -1,7 +1,7 @@
 import { SkinChanger } from "./SkinChanger.js";
 
 let skinStyle = document.querySelector("#skin"),
-    skins = ["gold","peach","basic","win95","mech","scifi","gamble","industrial","bones"];
+    skins = ["gold","peach","basic","win95","mech","scifi","gamble","industrial","bones","moiree"];
 
 
 window.skinChanger = new SkinChanger(skinStyle, skins, "./src/css/");
